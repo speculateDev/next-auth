fe0852b3f3ddf05b6dfe2ed3954beb7f94d9b550
Speculate Dev <sida3li777@gmail.com>
2025-06-28 18:37:32 +0100

    DB: @neondatabase/serverless instead of prisma

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,30 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
+
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
+
+// export const db = globalThis.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") {
+//   globalThis.prisma = db;
+// }
+import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
+
+config();
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  var sql: ReturnType<typeof neon> | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+// Ensure DATABASE_URL is defined
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined in the environment variables.");
+}
+
+export const sql = globalThis.sql || neon(process.env.DATABASE_URL);
 
 if (process.env.NODE_ENV !== "production") {
-  globalThis.prisma = db;
+  globalThis.sql = sql;
 }
