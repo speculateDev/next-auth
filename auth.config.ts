f8cf309cc8e32781970d4b5cd80375be950432d6
Speculate Dev <sida3li777@gmail.com>
2025-06-29 19:03:02 +0100

    Auth: added github, google providers into config

--- auth.config.ts
+++ auth.config.ts
@@ -2,6 +2,8 @@
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 import { LoginSchema, User } from "./schemas/index";
 import { getUserByEmail } from "./data/user";
@@ -9,6 +11,14 @@ import { getUserByEmail } from "./data/user";
 // Notice this is only an object, not a full Auth.js instance
 export default {
   providers: [
+    Github({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
