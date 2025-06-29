9bf03ae277af9fe562e1952509455a49badf11cb
Speculate Dev <sida3li777@gmail.com>
2025-06-29 11:55:31 +0100

    Auth: credentials log in applied

--- app/actions/login.tsx
+++ app/actions/login.tsx
@@ -2,14 +2,38 @@
 import * as z from "zod";
 import { LoginSchema } from "@/schemas";
 
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { AuthError } from "next-auth";
+
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
 
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  // await new Promise((resolve) => setTimeout(resolve, 2000));
 
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid Credentials!" };
+
+        default:
+          return { error: "Something went wrong!" };
+      }
+    }
+
+    throw error;
+  }
 };
--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,32 @@
-import GitHub from "next-auth/providers/github";
+// import GitHub from "next-auth/providers/github";
 import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+
+import { LoginSchema, User } from "./schemas/index";
+import { getUserByEmail } from "./data/user";
 
 // Notice this is only an object, not a full Auth.js instance
 export default {
-  providers: [GitHub],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const res = (await getUserByEmail(email)) as User[];
+
+          const user = res[0];
+          if (!user || !user.password) return null;
+
+          const isMatched = await bcrypt.compare(password, user.password);
+          if (isMatched) return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
