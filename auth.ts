1177e4be801fb779fc7421a7ae781f59060f7c24
Speculate Dev <sida3li777@gmail.com>
2025-06-29 13:28:27 +0100

    Auth: Extended session

--- auth.ts
+++ auth.ts
@@ -2,8 +2,32 @@ import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 import NeonAdapter from "@auth/neon-adapter";
 import { pool } from "./lib/db";
+import { getUserById } from "./data/user";
+import { User } from "./schemas";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = (await getUserById(token.sub)) as User[];
+      if (!existingUser) return token;
+
+      token.role = existingUser[0].role;
+      return token;
+    },
+  },
   adapter: NeonAdapter(pool),
   session: { strategy: "jwt" },
   ...authConfig,
