1d4a7dcb8781d3ddc675e47864e34532abaf543a
Speculate Dev <sida3li777@gmail.com>
2025-06-30 20:57:01 +0100

    Reset-Token: generate by mail

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -2,6 +2,7 @@ import { getVerificationTokenByEmail } from "@/data/verificationToken";
 import { v4 as uuid } from "uuid";
 import { sql } from "./db";
 import { Token } from "@/schemas";
+import { getPasswordResetTokenByEmail } from "@/data/resetToken";
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuid();
@@ -17,4 +18,20 @@ export const generateVerificationToken = async (email: string) => {
     (await sql`INSERT INTO VerificationToken(email, token, expires) VALUES(${email}, ${token}, ${expires}) RETURNING *`) as Token[];
 
   return verificationToken[0];
+};
+
+export const generateResetToken = async (email: string) => {
+  const token = uuid();
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getPasswordResetTokenByEmail(email);
+
+  if (existingToken) {
+    await sql`DELETE FROM ResetToken WHERE id = ${existingToken.id}`;
+  }
+
+  const resetToken =
+    (await sql`INSERT INTO ResetToken (email, token, expires) VALUES(${email}, ${token}, ${expires}) RETURNING *`) as Token[];
+
+  return resetToken[0];
 };
