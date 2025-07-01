704175a5297a233215a7b28da1cfcd63501ca15a
Speculate Dev <sida3li777@gmail.com>
2025-07-01 13:24:11 +0100

    2FA: generate token

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,8 +1,10 @@
+import crypto from "crypto";
 import { getVerificationTokenByEmail } from "@/data/verificationToken";
 import { v4 as uuid } from "uuid";
 import { sql } from "./db";
 import { Token } from "@/schemas";
 import { getPasswordResetTokenByEmail } from "@/data/resetToken";
+import { getTwoFactorTokenByEmail } from "@/data/towFactorToken";
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuid();
@@ -34,4 +36,22 @@ export const generateResetToken = async (email: string) => {
     (await sql`INSERT INTO ResetToken (email, token, expires) VALUES(${email}, ${token}, ${expires}) RETURNING *`) as Token[];
 
   return resetToken[0];
+};
+
+export const generateTwoFactorToken = async (email: string) => {
+  const token = crypto.randomInt(100_000, 1_000_000).toString();
+
+  //TODO: change to 15min
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getTwoFactorTokenByEmail(email);
+
+  if (existingToken) {
+    await sql`DELETE FROM twoFactorToken WHERE id = ${existingToken.id}`;
+  }
+
+  const twoFactorToken =
+    (await sql`INSERT INTO twoFactorToken (email, token, expires) VALUES(${email}, ${token}, ${expires}) RETURNING *`) as Token[];
+
+  return twoFactorToken[0];
 };
