896f4cb09a4ad515bc8ca98ecbeb2b36cefc653b
Speculate Dev <sida3li777@gmail.com>
2025-07-02 16:19:24 +0100

    init-UI: sonner toast added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default async function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           {children}
+          <Toaster richColors />
         </body>
       </html>
     </SessionProvider>
