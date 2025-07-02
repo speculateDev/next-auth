ae097dd11c34fc8bcd5209f48772d02625e541d8
Speculate Dev <sida3li777@gmail.com>
2025-07-02 17:03:31 +0100

    Role: api + action integrated in UI

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -2,6 +2,7 @@
 
 import RoleGate from "@/app/_components/auth/role-gate";
 import FormSuccess from "@/app/_components/form-success";
+import { admin } from "@/app/actions/admin";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { toast } from "sonner";
@@ -25,6 +26,17 @@ export default function AdminPage() {
     });
   };
 
+  const onServerActionClick = () => {
+    admin().then((data) => {
+      if (data.error) {
+        toast.error(data.error);
+      }
+      if (data.success) {
+        toast.success(data.success);
+      }
+    });
+  };
+
   return (
     <Card className="w-[600px] ">
       <CardHeader>
@@ -41,7 +53,7 @@ export default function AdminPage() {
         </div>
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
           <p className="text-sm font-medium ">Admin-only Server action</p>
-          <Button>Click to test</Button>
+          <Button onClick={onServerActionClick}>Click to test</Button>
         </div>
       </CardContent>
     </Card>
