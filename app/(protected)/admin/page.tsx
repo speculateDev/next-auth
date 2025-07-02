86a3de2d75a5344e42ce9e39d39563d8699729ef
Speculate Dev <sida3li777@gmail.com>
2025-07-02 14:54:00 +0100

    Role: client side

---
+++ app/(protected)/admin/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { useCurrentRole } from "@/hooks/use-current-role";
+
+export default function AdminPage() {
+  const role = useCurrentRole();
+
+  return <div>Current role : {role}</div>;
+}
