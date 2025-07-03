16adacaafe81eca0d98d91f73d3338e4262fc229
Speculate Dev <sida3li777@gmail.com>
2025-07-03 15:11:34 +0100

    Settings: update remaining fields using

--- app/actions/settings.ts
+++ app/actions/settings.ts
@@ -4,8 +4,11 @@ import * as z from "zod";
 
 import { SettingsSchema } from "@/schemas";
 import { sql } from "@/lib/db";
-import { getUserById } from "@/data/user";
+import { getUserByEmail, getUserById } from "@/data/user";
 import { currentUser } from "@/lib/auth";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
+import bcrypt from "bcryptjs";
 
 export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   const user = await currentUser();
@@ -15,7 +18,7 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   }
 
   // Check DB
-  const dbUser = getUserById(user.id);
+  const dbUser = await getUserById(user.id);
   if (!dbUser) {
     return { error: "Unauthorized" };
   }
@@ -29,12 +32,44 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
     values.isTwoFactoredEnabled = undefined;
   }
 
-  // const fieldsToUpdate = Object.entries(values)
-  //   // .filter(([_, value]) => value !== undefined)
-  //   .map(([key, value]) => sql`${key} = ${value}`)
-  //   .reduce((acc, part) => sql`${acc}, ${part}`);
+  if (values.email && user.email !== values.email) {
+    const existingUser = await getUserByEmail(values.email);
 
-  // await sql`UPDATE users SET ${fieldsToUpdate} WHERE id = ${user.id}`;
+    // check if email is already used (not one's email)
+    if (existingUser && existingUser.id !== user.id) {
+      return { error: "email already in use!" };
+    }
+
+    const verficationToken = await generateVerificationToken(values.email);
+
+    await sendVerificationEmail(verficationToken.email, verficationToken.token);
+
+    return { success: "Verification email sent!" };
+  }
+
+  if (values.password && values.newPassword && dbUser.password) {
+    const isMatch = await bcrypt.compare(values.password, dbUser.password);
+
+    if (!isMatch) {
+      return { error: "Incorrect password!" };
+    }
+
+    const hashedPass = await bcrypt.hash(values.password, 10);
+    values.password = hashedPass;
+    values.newPassword = undefined;
+  }
+
+  // Update DB
+  await sql`
+    UPDATE users
+    SET
+      name = ${values.name}, 
+      email = ${values.email}, 
+      password = ${values.password}, 
+      "isTwoFactorEnabled" = ${values.isTwoFactoredEnabled}, 
+      role = ${values.role}
+    WHERE id = ${user.id}  
+  `;
 
   return { success: "Settings Updated successfully!" };
 };
