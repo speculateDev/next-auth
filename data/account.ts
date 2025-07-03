fc328ecdc6cfc27cafd1a769afdea5dc07792d5c
Speculate Dev <sida3li777@gmail.com>
2025-07-03 13:57:08 +0100

    typo: data.getAccountByUserId

--- data/account.ts
+++ data/account.ts
@@ -2,7 +2,7 @@ import { sql } from "@/lib/db";
 
 export const getAccountByUserId = async (userId: string) => {
   try {
-    const res = await sql`SELECT * FROM accounts WHERE id = ${userId}`;
+    const res = await sql`SELECT * FROM accounts WHERE "userId" = ${userId}`;
 
     if (!Array.isArray(res) || !res.length) {
       return null;
